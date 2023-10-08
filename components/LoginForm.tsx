1d44044c96a9a24152cf15e19d5903115d8bae22
rishabh-j-23 <rjain2311200@gmail.com>
2023-10-08 20:40:21 +0530

    bug fix

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -19,7 +19,7 @@ const LoginForm = () => {
             setCredError(true);
             return;
         }
-        await axios.patch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+        await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
             username: username,
             password: password,
             email: email
