7e934bf521c420b86efd598fcf5d619df5c5952d
rishabh-j-23 <rjain2311200@gmail.com>
2023-10-09 21:20:48 +0530

    env fix

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -20,7 +20,7 @@ const ProfilePage = () => {
 
     useEffect(() => {
         var token = localStorage.getItem('sessionToken');
-        axios.get(`${process.env.API_URL}/user?token=${token}`).then(res => {
+        axios.get(`${process.env.NEXT_PUBLIC_API_URL}/user?token=${token}`).then(res => {
             setUsername(res.data['username']);
             setEmail(res.data['email']);
         }).catch(err => console.log(err))
