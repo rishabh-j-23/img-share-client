b6ca6d4a79411b95504b808eeb94779c120bff26
rishabh-j-23 <rjain2311200@gmail.com>
2023-10-11 11:30:03 +0530

    bugfix

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -9,7 +9,7 @@ const Register = () => {
 
     return (
         <div className='h-screen flex items-center justify-center '>
-            <div className='border-solid border-white  bg-slate-50 border-l rounded-md m-[3%] w-2/5'>
+            <div className='border-solid border-white  bg-slate-50 border-l rounded-md m-[3%] lg:w-2/5'>
                 <RegisterForm />
             </div>
         </div>
